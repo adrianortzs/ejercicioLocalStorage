f64b4382a05fb4aae69ebc320d12ee2fff629bf1
unknown <carloschinchillamoreno@gmail.com>
2023-10-13 01:22:54 +0200

    Se guardan todos los datos y se pintan en pantalla

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let formulario = document.querySelector("form");
 
+let listaUsers = [];
+localStorage.setItem("listaUsers", JSON.stringify(listaUsers));
+
 formulario.addEventListener("submit", function(event) {
 	event.preventDefault();
 
@@ -7,9 +10,26 @@ formulario.addEventListener("submit", function(event) {
     const email = event.target.email.value;
     const mesaje = event.target.mensaje.value;
     const imagen = event.target.imagen.value;
+    
+    let pintar = `nombre:${nombre}\n
+                    email:${email}\n
+                    mensaje: ${mesaje}\n
+                    imagen:${imagen}\n`
 
-    const usuario =[{nombre:`${nombre}`},{email:`${email}`}, {mensaje: `${mesaje}`}, {imagen:`${imagen}`}]
-    localStorage.setItem("user", JSON.stringify(usuario));
+    let p = document.createTextNode(pintar);
+    document.body.appendChild(p);
 
-    console.log(usuario)
+
+    const usuario ={
+        nombre:`${nombre}`,
+        email:`${email}`, 
+        mensaje: `${mesaje}`, 
+        imagen:`${imagen}`
+    }
+    
+    let nuevoUser = JSON.parse(localStorage.getItem("listaUsers"));
+    
+    nuevoUser.push(usuario);
+    localStorage.setItem("listaUsers", JSON.stringify(nuevoUser));
+
 })
